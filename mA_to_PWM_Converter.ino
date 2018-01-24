e72fb09ed8f0ab88536c018727a65c5df6e83a39
titetil <gtetil@us.tiauto.com>
2018-01-24 17:57:45 -0500

    - Added soft start.

--- mA_to_PWM_Converter.ino
+++ mA_to_PWM_Converter.ino
@@ -1,5 +1,5 @@
 
-int aiPin = A0;
+int aiPin = A1;
 int pwmPin = 3;
 int maxOnTime = 128; //microseconds
 int onTime = 0;
@@ -10,6 +10,30 @@ void setup() {
   ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
   ADCSRA |= bit (ADPS2); 
   pinMode(pwmPin, OUTPUT);
+
+  //soft start
+  delay(5000);
+  aiRead = analogRead(aiPin) >> 3;
+  if (aiRead > 10) {
+    for (int i = 0; i < 15000; i++) {
+      digitalWrite(pwmPin, HIGH);
+      delayMicroseconds(20);
+      digitalWrite(pwmPin, LOW);
+      delayMicroseconds(108);
+    }
+    for (int i = 0; i < 15000; i++) {
+      digitalWrite(pwmPin, HIGH);
+      delayMicroseconds(64);
+      digitalWrite(pwmPin, LOW);
+      delayMicroseconds(64);
+    }
+    for (int i = 0; i < 15000; i++) {
+      digitalWrite(pwmPin, HIGH);
+      delayMicroseconds(110);
+      digitalWrite(pwmPin, LOW);
+      delayMicroseconds(18);
+    }
+  }
 
 }
 
